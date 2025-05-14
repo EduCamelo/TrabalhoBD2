5a6d178004328032c977f47e45af917bcc94d3b4
Pimenta15 <jpmp150404@gmail.com>
2025-05-14 10:21:33 -0300

    usuarios ajustados

--- sql/04_users_permissions.sql
+++ sql/04_users_permissions.sql
@@ -1,18 +1,22 @@
-
-
-CREATE USER 'admin'@'localhost' IDENTIFIED BY 'senha_admin';
-GRANT ALL PRIVILEGES ON ecommerce_jogos.* TO 'admin'@'localhost';
-
-CREATE USER 'gerente'@'localhost' IDENTIFIED BY 'senha_gerente';
-GRANT SELECT, UPDATE, DELETE 
+-- Administrador: todas as permissões
+CREATE USER 'admin_ecom'@'localhost' IDENTIFIED BY 'senha_admin';
+GRANT ALL PRIVILEGES 
   ON ecommerce_jogos.* 
-  TO 'gerente'@'localhost';
+  TO 'admin_ecom'@'localhost';
 
+-- Gerente: SELECT/UPDATE/DELETE/EXECUTE no schema inteiro
+CREATE USER 'gerente_ecom'@'localhost' IDENTIFIED BY 'senha_gerente';
+GRANT SELECT, UPDATE, DELETE, EXECUTE
+  ON ecommerce_jogos.* 
+  TO 'gerente_ecom'@'localhost';
 
+-- Funcionário: INSERT/SELECT só em `venda` + EXECUTE no schema
 CREATE USER 'func_ecom'@'localhost' IDENTIFIED BY 'senha_func';
 GRANT INSERT, SELECT
-  ON ecommerce_jogos.venda
+  ON ecommerce_jogos.venda 
   TO 'func_ecom'@'localhost';
-
+GRANT SELECT, EXECUTE
+  ON ecommerce_jogos.* 
+  TO 'func_ecom'@'localhost';
 
 FLUSH PRIVILEGES;
